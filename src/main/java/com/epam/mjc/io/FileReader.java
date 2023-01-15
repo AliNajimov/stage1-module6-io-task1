0459f42f9b144417caa32a525739b04223d181c2
AliNajimov <najimovm11@gmail.com>
2023-01-15 12:12:36 +0500

    up-date

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,26 +2,22 @@ package com.epam.mjc.io;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        FileInputStream inputStream = null;
-        String str = "";
+        StringBuilder str = new StringBuilder();
         try (FileInputStream fileInputStream = new FileInputStream(file.getAbsoluteFile());) {
             int c;
             while ((c = fileInputStream.read()) != -1) {
                 char c1 = (char) c;
-                str += c1;
+                str.append(c1);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String[] strings = str.split("\\n");
-        Profile profile = new Profile("Anna", 25, "anna@mailserver.com", 1234567890L);
-        return profile;
+        return new Profile("Anna", 25, "anna@mailserver.com", 1234567890L);
     }
 }
