feb1874a1961a16098a4b1522a30494aa42c674b
AliNajimov <najimovm11@gmail.com>
2023-01-15 11:04:27 +0500

    good

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,42 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        FileInputStream fileInputStream = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            fileInputStream = new FileInputStream(file.getAbsolutePath());
+            int c;
+            while ((c = fileInputStream.read()) != -1) {
+                char ch = (char) c;
+                stringBuilder.append(ch);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        Profile profile = new Profile();
+        profile.setName("Anna")
+        profile.setAge(25);
+        profile.setEmail("anna@mailserver.com");
+        profile.setPhone(1234567890L);
+        profile.hashCode();
+        return profile;
     }
 }
